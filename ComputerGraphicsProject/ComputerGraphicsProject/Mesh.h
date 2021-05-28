cd57843af117dec5511eacf95ffd279a63878f4b
Георги Донков <gesho19@abv.bg>
2021-05-28 14:51:02 +0300

    Set up

---
+++ ComputerGraphicsProject/ComputerGraphicsProject/Mesh.h
@@ -0,0 +1,54 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <glad/glad.h> // holds all OpenGL type declarations
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Shader.h"
+
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Vertex {
+    // position
+    glm::vec3 Position;
+    // normal
+    glm::vec3 Normal;
+    // texCoords
+    glm::vec2 TexCoords;
+    // tangent
+    glm::vec3 Tangent;
+    // bitangent
+    glm::vec3 Bitangent;
+};
+
+struct TextureInfo {
+    unsigned int id;
+    string type;
+    string path;
+};
+
+class Mesh {
+public:
+    // mesh Data
+    vector<Vertex>       vertices;
+    vector<unsigned int> indices;
+    vector<TextureInfo>      textures;
+    unsigned int VAO;
+
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<TextureInfo> textures);
+
+    // render the mesh
+    void Draw(Shader& shader);
+
+private:
+    // render data 
+    unsigned int VBO, EBO;
+
+    // initializes all the buffer objects/arrays
+    void setupMesh();
+};
+#endif
